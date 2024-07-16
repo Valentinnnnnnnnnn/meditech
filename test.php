3c38d3748480cdaae69ab6329aa36e54762ef08e
Valentin <valentin@LT-VA-2024.local>
2024-07-16 14:12:34 +0200

    Utilisation de variables d environnement

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+echo "DB_HOST: " . getenv('DB_HOST') . "<br>";
+echo "DB_NAME: " . getenv('DB_NAME') . "<br>";
+echo "DB_USER: " . getenv('DB_USER') . "<br>";
+echo "DB_PASS: " . getenv('DB_PASS') . "<br>";
+echo "DB_CHARSET: " . getenv('DB_CHARSET') . "<br>";
+?>
