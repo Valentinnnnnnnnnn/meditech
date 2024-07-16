712df235ab6f4a3926229e1028716c8491ddee78
Valentin <valentin@LT-VA-2024.local>
2024-07-16 15:55:46 +0200

    Utilisation de variables d environnement

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -29,12 +29,14 @@ if (!isset($_SESSION['email']) or !isset($_SESSION['pass'])) {
         </div>
     </nav>
 
-    <div class="container">
-        <div class="box1"></div>
-        <div class="box2"></div>
-        <div class="box3"></div>
-        <div class="box4"></div>
-        <div class="box5"></div>
+    <div class="wrapper">
+        <div class="container">
+            <div class="box1"></div>
+            <div class="box2"></div>
+            <div class="box3"></div>
+            <div class="box4"></div>
+            <div class="box5"></div>
+        </div>
     </div>
     
     </body>
